2fa71b8e3e38fd68eba8c486605f4828246ceb73
Jeeva <cjeevanandham@gmail.com>
2024-09-05 08:33:56 +0530

    task_5 optimized code

--- task_5/task_5-program2.c
+++ task_5/task_5-program2.c
@@ -1,37 +1,11 @@
- #include <stdio.h>
-
+#include <stdio.h>
 int main()
 {
-    unsigned long int num; //variables.
-    int i = 0,arr[32] = {0},j = 15,ret = 0,sum = 0,base = 1;
-    printf("Enter the decimal number: ");
-    scanf("%d",&num); //read the num.
-    for(i=0;i<32;i++)  //loop.
-    {
-        if(num & (1 << i))
-        {
-            if(i >= 15 && i <= 23) //adding one.
-            {
-                arr[j++] = 1;
-            }
-        }
-        else
-        {
-            if(i >= 15 && i <= 23)
-            {
-                arr[j++] = 0; //adding zero.
-            }
-        }
-    }
-    for(j = 15;j<=23;j++)
-    {
-        if(arr[j] & 1)
-        {
-            sum += base; //binary equalant decimal number.
-        }
-        base *= 2;
-    }
-    printf("The extracted number is %d\n",sum); //output.
-    return 0;
+	unsigned long int num;
+	int ret=0,ret1=0;
+	printf("Enter the Number: ");
+	scanf("%ld",&num);
+	ret = (unsigned)num >> 15;
+	ret1 = ret & ((1 << 9)-1);
+	printf("The extracted number is %d\n",ret1);
 }
-//End of program.
